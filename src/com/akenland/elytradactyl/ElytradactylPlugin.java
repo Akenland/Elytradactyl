39e15760e3637b937e2371cba0eb4b92613ed48c
Kade <kade@aceofkade.com>
2024-12-29 23:52:02 -0500

    support MC 1.20+

--- src/com/akenland/elytradactyl/ElytradactylPlugin.java
+++ src/com/akenland/elytradactyl/ElytradactylPlugin.java
@@ -9,9 +9,11 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion.Target;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 /**
@@ -21,6 +23,7 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author;
 @Description(value = "Mobs use Elytra to glide!")
 @Author(value = "Kade")
 @Website(value = "https://plugins.akenland.com")
+@ApiVersion(Target.v1_20)
 public final class ElytradactylPlugin extends JavaPlugin {
 
 	/** The ID number of the task for this plugin. */
@@ -42,7 +45,7 @@ public final class ElytradactylPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		// Main command
-		//getCommand("elytradactyl").setExecutor(new ElytradactylCommands(this));
+		// getCommand("elytradactyl").setExecutor(new ElytradactylCommands(this));
 
 		// Run all reload tasks
 		reload();
